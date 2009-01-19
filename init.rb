a887d517883362c6157d28df35c9cbe3a1224038
Laszlo Bacsi <lackac@lackac.hu>
2009-01-19 15:33:08 +0100

    Added eager loading of potential signature methods

--- init.rb
+++ init.rb
@@ -2,6 +2,13 @@ require 'openssl'
 require 'oauth'
 require 'oauth/consumer'
 require 'oauth/request_proxy/action_controller_request'
+# require supported signature methods since they wouldn't be autoloaded
+%w{
+  hmac-md5 hmac-rmd160 hmac-sha1
+  md5 plaintext rsa-sha1 sha1
+}.each do |method|
+  require "oauth/signature/#{method.tr('-', '/')}"
+end
 
 require 'gadgeteer'
 
